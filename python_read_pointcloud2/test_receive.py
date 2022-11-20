be6a9dc0553599bc65badd3913b656c29edd8c19
zyqdragon <zhangjdr@163.com>
2022-11-20 09:54:21 +0800

    Add files via upload

--- python_read_pointcloud2/test_receive.py
+++ python_read_pointcloud2/test_receive.py
@@ -4,23 +4,27 @@
 import rospy
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs import point_cloud2
+import numpy as np
 
 class PointCloudSubscriber(object):
     def __init__(self): # -> None:
         #self.sub = rospy.Subscriber("pointcloud_topic",
         #                             PointCloud2,
         #                             self.callback, queue_size=5)
-        self.sub = rospy.Subscriber("/livox/lidar2",
+        self.sub = rospy.Subscriber("/livox/lidar2",   # ros topic name
                                      PointCloud2,
                                      self.callback, queue_size=5)
     def callback(self, msg):
         assert isinstance(msg, PointCloud2)
 
-        # gen=point_cloud2.read_points(msg,field_names=("x","y","z"))
-        points = point_cloud2.read_points_list(
-            msg, field_names=("x", "y", "z"))
+        gen=point_cloud2.read_points(msg,field_names=("x","y","z"))
+        for point in gen:
+            print(point)
+        points = point_cloud2.read_points_list(msg, field_names=("x", "y", "z"))
+        # print(points)
 
-        print(points)
+        point = np.array([point for point in gen])
+        print("----points---",points,"-----points[1][2]=",points[1][2])
 
 
 if __name__ =='__main__':
